3ebb4d47d043a44afe04ffd1187219117b0138be
swinitdept <59125418+swinitdept@users.noreply.github.com>
2019-12-21 17:50:47 -0800

    Add files via upload

--- SwinTicketGUI.py
+++ SwinTicketGUI.py
@@ -1,5 +1,6 @@
 # Import Stuff Here
 from tkinter import *
+import os
 
 class Window(Frame):
     def __init__(self, master = None):
@@ -9,21 +10,29 @@ class Window(Frame):
 
         self.init_window()
 
+
+
+
     def init_window(self):
         self.master.title("Swin Ticket Support")
         self.pack(fill=BOTH, expand=1)
-        quitButton = Button(self, text="Quit", command=self.client_exit, width=10, height=2, bg="red")
-        quitButton.place(x=50, y=150)
+
+    ### BUTTONS ###
+
 
         # Title
         samepleTxt = Label(self, text="Swinomish Casino and Lodge Support Ticket")
         samepleTxt.pack()
 
         # Name field
+        v = StringVar()
         samepleTxt = Label(self, text="Name:")
         samepleTxt.place(x=10,y=40)
         name_field = Entry(self)
         name_field.place(x=200, y=40)
+        name_field.insert(0, os.getlogin())
+        testVar = name_field
+        print(testVar)
 
         # Department field
         samepleTxt = Label(self, text="Department:")
@@ -44,8 +53,20 @@ class Window(Frame):
         problem_field = Entry(self)
         problem_field.place(x=200, y=130)
 
+
+        quitButton = Button(self, text="Quit", command=self.client_exit, width=10, height=2, bg="red")
+        quitButton.place(x=50, y=150)
+
+        submitButton = Button(self, text="Submit", command=self.client_submit, width=10, height=2, bg="lightgreen")
+        submitButton.place(x=50, y=250)
+
     def client_exit(self):
         exit()
+
+    def client_submit(self):
+        print(Window.name_field.get())
+        #file = open("%s.txt" % os.getlogin(), "a")
+        #file.write("Name: " + self.entry.get())
 
     #def showText(self):
         #text = Label(self, text="Hello")
